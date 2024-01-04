81f01b8b771a3f5db2d7c83aa32015b09ff5275e
Yuri Caruso <ycaruso@gmail.com>
2024-01-04 08:39:50 -0300

    feat: add select component initial

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import Icon from "../Icon/Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+import { COLORS } from "../../constants";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <SelectStyled label={label} value={value} onChange={onChange}>
+        {children}
+      </SelectStyled>
+      <IconWrapper>
+        <Icon id={"chevron-down"} size={24} />
+      </IconWrapper>
+    </SelectWrapper>
   );
 };
+
+const SelectWrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const SelectStyled = styled.select`
+  padding: 12px 16px;
+  border-radius: 8px;
+  border: none;
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  right: 0px;
+  margin-top: auto;
+  margin-bottom: auto;
+  width: fit-content;
+  height: fit-content;
+  pointer-events: none;
+`;
 
 export default Select;
